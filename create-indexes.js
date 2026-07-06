9037da58e761ab2ac9d600455d9e0489fbb06dad
rrchai <73901500+rrchai@users.noreply.github.com>
2026-07-06 19:02:41 +0000

    refactor: rename collated index suffix to _collated (MG-912)

--- create-indexes.js
+++ create-indexes.js
@@ -73,7 +73,7 @@ for (let collection of collections) {
         }
     }
     for (let index of (collection.collatedIndexes || [])) {
-        const name = Object.keys(index).map(k => k + '_' + index[k]).join('_') + '_ci';
+        const name = Object.keys(index).map(k => k + '_' + index[k]).join('_') + '_collated';
         print('Creating collated index: ' + name);
         results = db[collection.name].createIndex(index, { name: name, collation: collation });
         if (results && results.ok === 1) {
